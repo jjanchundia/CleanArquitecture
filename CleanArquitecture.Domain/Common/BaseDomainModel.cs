c814487df374d3734597ec2a806a2736a8f6e12d
Jonathan Anchundia <jjanchundia92@gmail.com>
2024-09-13 16:17:17 -0500

    Agregar archivos de proyecto.

---
+++ CleanArquitecture.Domain/Common/BaseDomainModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CleanArquitecture.Domain.Common
+{
+    //Clase abstracta para evitar instancias o creacion de objetos  
+    //directamente, solo se usa para hacer herencia
+    public abstract class BaseDomainModel
+    {
+        public int Id { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public string? CreatyBy { get; set; }
+        public  DateTime LastModifiedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
